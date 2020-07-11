60696f33b08ddfb7d496f74ddff87cbe311e64f3
wendelleloi <wendelleloi2@gmail.com>
2020-07-11 13:21:11 -0300

    :sparkles: Add user

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/user';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'hello world' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Wendell',
+    email: 'wendelleloi2@gmail.com',
+    password_hash: '123456',
+  });
+  return res.json(user);
+});
 
 export default routes;
